66d215a03fc4c069fad60197b147d15d346812fd
Atick Faisal <atickfaisal@gmail.com>
2023-09-08 22:24:49 +0300

    Add image classification endpoint

--- main.py
+++ main.py
@@ -12,13 +12,42 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
+
+import io
+import os
+import numpy as np
+from PIL import Image
 import tensorflow as tf
-from fastapi import FastAPI
+from starlette.responses import JSONResponse
+from fastapi import FastAPI, File, UploadFile
+
+
+if "GOOGLE_CLOUD_PROJECT" in os.environ:
+    print("Running on Google Cloud")
+    model = tf.keras.models.load_model("gs://jetpack-models/cats_and_dogs")
+else:
+    print("Running on a local machine")
+    model = tf.keras.models.load_model("models/cats_and_dogs")
+
+class_names = ["cats", "dogs"]
 
 app = FastAPI()
 
-model = tf.keras.models.load_model("gs://jetpack-models/cats_and_dogs")
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/predict")
+async def predict(file: UploadFile):
+    image = Image.open(io.BytesIO(await file.read()))
+    image = image.resize((160, 160))
+    image = np.array(image)
+    image = np.expand_dims(image, axis=0)
+
+    predictions = model.predict_on_batch(image).flatten()
+    predictions = tf.nn.sigmoid(predictions)
+    predictions = tf.where(predictions < 0.5, 0, 1)
+
+    return JSONResponse({"class": class_names[predictions[0]]})
