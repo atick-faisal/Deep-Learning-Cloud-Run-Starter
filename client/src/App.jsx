b8f4927549711fa57269e5746e703ca6fdd23bac
Atick Faisal <atickfaisal@gmail.com>
2023-09-09 17:14:06 +0300

    Finish converting to pwa

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import placeholder from "./assets/placeholder.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [prediction, setPrediction] = useState("None");
+    const [loading, setLoading] = useState(false);
+    const [image, setImage] = useState(placeholder);
+    const [imageFile, setImageFile] = useState(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const uploadImage = async () => {
+        setLoading(true);
+        const formData = new FormData();
+        formData.append("image", imageFile);
+        try {
+            const response = await fetch("/predict", {
+                method: "POST",
+                body: formData,
+            });
+            if (response.ok) {
+                const data = await response.json();
+                setPrediction(data.class);
+                console.log(data);
+            } else {
+                console.error("Server Error: ", response.status);
+            }
+        } catch (error) {
+            console.error("Error: ", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const onSelectImage = (e) => {
+        const file = e.target.files?.[0];
+        if (file) {
+            setImageFile(file);
+            setPrediction("None");
+            const reader = new FileReader();
+            reader.onload = (event) => {
+                if (event.target) {
+                    setImage(event.target.result);
+                }
+            };
+            reader.readAsDataURL(file);
+        } else {
+            setImage(placeholder);
+        }
+    };
+
+    return (
+        <div className="container">
+            <div className="content">
+                <h4 className="label">Prediction: {prediction}</h4>
+                <div>
+                    <input
+                        accept="image/*"
+                        className="SelectImage"
+                        style={{ display: "none" }}
+                        id="image-selector"
+                        multiple
+                        type="file"
+                        onChange={onSelectImage}
+                    />
+                    <label htmlFor="image-selector">
+                        <img
+                            style={{ width: "100%" }}
+                            src={image}
+                            alt="image"
+                        />
+                    </label>
+                </div>
+                <button aria-busy={loading} onClick={uploadImage}>
+                    Upload
+                </button>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,53 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        VitePWA({
+            // add this to cache all the imports
+            workbox: {
+                globPatterns: ["**/*"],
+            },
+            // add this to cache all the
+            // static assets in the public folder
+            includeAssets: ["**/*"],
+            manifest: {
+                short_name: "Jetpack AI",
+                name: "Jetpack AI",
+                icons: [
+                    {
+                        src: "icons/manifest-icon-192.maskable.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                        purpose: "any",
+                    },
+                    {
+                        src: "icons/manifest-icon-192.maskable.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                        purpose: "maskable",
+                    },
+                    {
+                        src: "icons/manifest-icon-512.maskable.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "any",
+                    },
+                    {
+                        src: "icons/manifest-icon-512.maskable.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "maskable",
+                    },
+                ],
+                start_url: ".",
+                display: "standalone",
+                theme_color: "#5e35b1",
+                background_color: "#fefefe",
+            },
+        }),
+    ],
+});
